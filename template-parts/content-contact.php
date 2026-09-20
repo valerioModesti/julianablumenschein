4f39fcc91b1a6c9dba11c53842ebff7b174b4b70
Valerio Modesti <valeriomodesti@MBP-di-Valerio.fritz.box>
2026-09-21 00:01:49 +0200

    chore: removes comments

--- template-parts/content-contact.php
+++ template-parts/content-contact.php
@@ -6,18 +6,5 @@
         ?>
         <h2><?php echo $queried_post->post_title; ?></h2>
         <?php echo do_shortcode( $queried_post->post_content ); ?>
-        <?php
-        /*
-         $query = new WP_Query(array(
-        'post_type' => 'contact',
-        'post_status' => 'publish'
-    ));    
-    while ($query->have_posts()) {    
-            $query->the_post();
-            $post_id = get_the_ID();
-            $linktext = get_post_meta( $post_id, 'rw_linktext', true );
-            $url = get_post_meta( $post_id, 'rw_url', true );
-        */
-        ?>
-    </div>
-</div>
+    </div><!-- .section-content.centered -->
+</div><!-- #contact -->
